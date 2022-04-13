9370dfa42f427a0eb9c33622d09333a2f42d3fab
elkana-m <munganga.elkana@yahoo.fr>
2022-04-13 16:29:41 -0500

    Initial Commit

---
+++ EFCoreConsoleDBFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreConsoleDBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
